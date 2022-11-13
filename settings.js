c6e87e2d73003292034e6de9b269a130192a05af
Pain729 <101877548+Pain729@users.noreply.github.com>
2022-11-13 19:18:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 global.hituet = 0
-global.gopayno = "+7 977 412-71-81"
-global.danano = "+7 977 412-71-81"
-global.shopeepayno = "+7 977 412-71-81"
-global.creator = "+79774127181@s.whatsapp.net"
+global.gopayno = "+7 991 473-36-27"
+global.danano = "+7 991 473-36-27"
+global.shopeepayno = "+7 991 473-36-27"
+global.creator = "+79914733627@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `📍𝙋𝙞𝙖𝙣×͜×`
@@ -15,9 +15,9 @@ global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '+7 977 412-71-81'
+global.ownerNomor = '+7 991 473-36-27'
 global.ownerName = 'Creator Pian'
-global.ownerNumber = ["+7 977 412-71-81@s.whatsapp.net"]
+global.ownerNumber = ["+7 991 473-36-27@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
     wait: 'Wait Sis Please be patient',
